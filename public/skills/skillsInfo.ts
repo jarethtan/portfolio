af717f89a501461d6299d05328d45787add412c0
jarethtan <jareth.tan@outlook.com>
2022-05-31 22:16:08 +0800

    update skills section content

--- public/skills/skillsInfo.ts
+++ public/skills/skillsInfo.ts
@@ -29,18 +29,18 @@ export const skillsTitle = [
 ];
 
 export const skillsContent = [
-  "I use HTML, CSS and JavaScript to build attractive and responsive WebApps.",
-  "To build fast and responsive Single Page Applications, ReactJS is go to library.",
-  "NextJS is my go-to React base framework for building full stack React applications.",
-  "Bootstrap is my go-to frontend framework to build fast and beautiful design.",
-  "A runtime environment for JavaScript language, I use this in most of my projects.",
+  "HTML, CSS and JavaScript are the fundamental building blocks for my WebApps",
+  "To build fast and responsive Single Page Applications, ReactJS is my go-to library.",
+  "NextJS is my go-to React base framework to build full stack React applications.",
+  "Bootstrap is my frontend framework of choice to build fast and responsive WebApps.",
+  "A runtime environment for JavaScript language, I use this in all JavaScript projects.",
   "A backend framework build on NodeJS, I use as server side with RESTful API.",
   "A non-SQL database, I use MongoDB when I want flexibility in storing my data.",
-  "I use Heroku when I am looking for intuitive and easy way to host my application.",
-  "For serverless and fast deployment, I use Vercel to host my NextJS applications",
-  "I use Typescript to improve readability and checking of my code more effectively.",
-  "Due to its ease-of-use and performance, I use redux for all my state management.",
-  "For my media storage and custom needs, cloudinary is my go-to image solution",
+  "I use Heroku when I am looking for an intuitive and easy way to host my application.",
+  "For serverless and fast deployment, I use Vercel to host my NextJS applications.",
+  "I use Typescript to improve readability and to check my code more effectively.",
+  "Due to its ease-of-use and performance, I use redux for all my state management needs.",
+  "For my media storage and custom needs, Cloudinary is my image solution of choice.",
 ];
 
 export const legend = ["Frontend", "Frontend", "Frontend", "Frontend", "Backend", "Backend", "Backend", "Deployment", "Deployment", "Tools", "Tools", "Tools"];
