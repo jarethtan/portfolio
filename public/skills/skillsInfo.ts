75508e586bec5f34ff5c6cafdadbb978ab17d99a
jarethtan <jareth.tan@outlook.com>
2022-05-31 22:20:20 +0800

    update skills section content

--- public/skills/skillsInfo.ts
+++ public/skills/skillsInfo.ts
@@ -30,16 +30,16 @@ export const skillsTitle = [
 
 export const skillsContent = [
   "HTML, CSS and JavaScript are the fundamental building blocks for my WebApps",
-  "To build fast and responsive Single Page Applications, ReactJS is my go-to library.",
+  "To build fast and responsive single page applications (SPA), ReactJS is my go-to library.",
   "NextJS is my go-to React base framework to build full stack React applications.",
   "Bootstrap is my frontend framework of choice to build fast and responsive WebApps.",
   "A runtime environment for JavaScript language, I use this in all JavaScript projects.",
   "A backend framework build on NodeJS, I use as server side with RESTful API.",
   "A non-SQL database, I use MongoDB when I want flexibility in storing my data.",
-  "I use Heroku when I am looking for an intuitive and easy way to host my application.",
+  "I use Heroku for an intuitive and easy way to host my application.",
   "For serverless and fast deployment, I use Vercel to host my NextJS applications.",
   "I use Typescript to improve readability and to check my code more effectively.",
-  "Due to its ease-of-use and performance, I use redux for all my state management needs.",
+  "Due to its ease-of-use and performance, I use redux for my state management needs.",
   "For my media storage and custom needs, Cloudinary is my image solution of choice.",
 ];
 
